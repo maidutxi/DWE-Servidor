d161b0182e43124a9557b09b3b17b0c703f310b1
maidutxi <147398914+maidutxi@users.noreply.github.com>
2024-09-16 13:39:43 +0200

    Update Ejer3.php

--- EjersFormularios/php/Ejer3.php
+++ EjersFormularios/php/Ejer3.php
@@ -4,9 +4,9 @@
 	$contraseña2=htmlspecialchars($_POST["contraseña2"]);
 	
 	if($contraseña==$contraseña2){
-		echo (htmlspecialchars($_POST["nombre"]. htmlspecialchars($_POST["email"]. ". Las contraseñas coinciden");
+		echo (htmlspecialchars($_POST["nombre"]. htmlspecialchars($_POST["email"] . ". Las contraseñas coinciden");
 	}
 	else{
-		echo (htmlspecialchars($_POST["nombre"]. htmlspecialchars($_POST["email"]. ".Las contraseñas no coinciden.");
+		echo (htmlspecialchars($_POST["nombre"]. htmlspecialchars($_POST["email"] . ".Las contraseñas no coinciden.");
 	}
 ?>
