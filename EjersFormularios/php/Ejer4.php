2e47df7909be15afbcfb1dfc7a910d3e68ea1b20
maider <mbelvermi23dw@ikzubirimanteo.com>
2024-09-17 08:50:31 +0200

    ejer4

--- EjersFormularios/php/Ejer4.php
+++ EjersFormularios/php/Ejer4.php
@@ -1,8 +1,17 @@
 <?php
    if($_SERVER["REQUEST_METHOD"]=="POST"){
-	$nombre=htmlspecialchars[$_POST["nombre"];
-	$apellido=htmlspecialchars[$_POST["apellido"];
-
-
+	$nombre=htmlspecialchars($_POST["nombre"]);
+	$apellido=htmlspecialchars($_POST["apellido"]);
+	$email=htmlspecialchars($_POST["email"]);
+	$telefono=htmlspecialchars($_POST["telefono"]);
+	
+	echo "<h1>Contacto agregado:</h1";
+	echo "Nombre: ". $nombre."<br>";
+	echo "apellido: ". $apellido."<br>";
+	echo "Email: ". $email."<br>";
+	echo "Telefono: " $telefono."<br>";
+   }
+   else{
+	echo "No se han enviado datos";
 
 ?>
