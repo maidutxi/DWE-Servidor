1150ffb64dca5d982bcc5cc1b87666192f3cfba7
maider <mbelvermi23dw@ikzubirimanteo.com>
2024-09-20 11:40:15 +0200

    ejer3Poo

---
+++ OOP bloque2/Ejer3.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer3 OOP</title>
+</head>
+<body>
+    <?php
+
+        class Registro {
+            private $nombre;
+            private $email;
+            private $contraseña;
+            private $repetirContraseña;
+
+
+            function __construct($nombre,$email,$contraseña,$repetirContraseña){
+                $this ->name=$name;
+                $this ->email=$email;
+                $this ->contraseña=$contraseña;
+                $this ->repetirContraseña=$repetirContraseña;
+            }
+
+            function set_name($nombre){
+                $this ->nombre= $nombre;
+            }
+
+            function set_email($email){
+                $this -> email=$email;
+            }
+
+            function set_contraseña($contraseña){
+                $this ->contraseña=$contraseña;
+            }
+
+            function set_contraseña2($repetirContraseña){
+                $this ->contraseña2=$repetirContraseña;
+            }
+
+            function get_nombre(){
+                return $this -> nombre;
+            }
+
+            function get_email(){
+                return $this -> email;
+            }
+
+            function get_contraseña(){
+                return $this -> contraseña;
+            }
+
+            function get_contraseña2(){
+                return $this -> contraseña2;
+            }
+
+            
+
+        }
+        
+        if($_SERVER["REQUEST_METHOD"]== "POST"){
+            $usuario = new Registro($_POST["nombre"],$_POST["email"],$_POST["contraseña"], $_POST["contraseña2"]);
+
+            
+
+            if($usuario->get_contraseña==$usuario->get_contraseña2){
+                echo $usuario->get_nombre . "<br>" . $usuario ->get_email . " Las contraseñas coinciden."
+            }
+            else{}
+            
+        }
+
+        
+    
+
+    ?>
+
+    <form  method="post" action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]); ?>">
+
+    </form>
+</body>
+</html>
