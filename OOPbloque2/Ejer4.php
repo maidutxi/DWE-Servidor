671d6b16599a9c4df4169ed3c1e719361b2ceab7
maidutxi <147398914+maidutxi@users.noreply.github.com>
2024-09-20 13:05:40 +0200

    Add files via upload

---
+++ OOPbloque2/Ejer4.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejer3 OOP</title>
+</head>
+<body>
+    <?php
+
+    class Registro {
+        private $nombre="";
+        private $apellido="";
+        private $email="";
+        private $telefono="";
+
+        function __construct($nombre, $apellido, $email, $telefono) {
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->email = $email;
+            $this->telefono = $telefono;
+        }
+
+        function get_nombre() {
+            return $this->nombre;
+        }
+
+        function get_email() {
+            return $this->email;
+        }
+
+        function get_apellido() {
+            return $this->apellido;
+        }
+
+        function get_telefono() {
+            return $this->telefono;
+        }
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $contacto = new Registro($_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["telefono"]);
+        echo("Nombre: ". $contacto-->get_nombre()."<br> Apellido: ".$contacto->get_apellido()."<br>Email: ".$contacto->get_email. "<br>Teléfono: ".$contacto->get_telefono());
+
+        
+    }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label>Nombre :</label><input type="text" name="nombre" required value="<?php echo $nombre;?>">
+        <br>
+        <label>Apellido:</label><input type="password" name="contraseña" required value="<?php echo $apellido;?>">
+        <br>
+        <label>Correo Electrónico:</label><input type="email" name="email" required value="<?php echo $email;?>">
+        <br>
+        
+        <label> Email:</label><input type="password" name="contraseña2" required value="<?php echo $telefono;?>">
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
